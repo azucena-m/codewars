d014614a139323f018957b0130bb6aa21aea48e1
Azucena Marroquin <atrevizo4@cnm.edu>
2024-03-12 09:25:54 -0600

    added another codewars kata

--- 7kyu/stringEndsWith.js
+++ 7kyu/stringEndsWith.js
@@ -8,3 +8,6 @@
 function solution(str, ending) {
     return str.endsWith(ending);
   }
+
+//   The endsWith() method checks whether a string ends with the characters of a specified string, returning true or false as appropriate.
+//   The function simply returns the result of str.endsWith(ending), which will be true if str ends with ending and false otherwise
